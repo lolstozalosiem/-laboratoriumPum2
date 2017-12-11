cd03a9afc32b024eeaafa6e5d03ea3f77f958352
unknown <student@IBLAB19N-07.ib.polsl.pl>
2017-12-11 16:37:45 +0100

    jeden

---
+++ app/src/main/java/com/example/student/gabor/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.student.gabor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+       // i. findViewById
+       // ii. setOnClickListener
+        //iii. getText.toString()
+
+
+
+
+
+
+
+    }
+}
